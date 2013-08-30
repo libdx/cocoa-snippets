6add281ae4c9f73f5d01ce665fe8a8410a523c64
Alexander Ignatenko <avi.cpp@gmail.com>
2013-08-30 12:40:21 +0300

    Add strongify macros

--- CocoaSnippets/Macros.h
+++ CocoaSnippets/Macros.h
@@ -6,4 +6,5 @@
 //  Copyright (c) 2013 Alexander Ignatenko. All rights reserved.
 //
 
-#define weakify(object, weakRef) __weak typeof(object) weakRef = object
+#define weakify(object, reference) __weak typeof(object) reference = object
+#define strongify(reference, object) __strong typeof(reference) object = reference
